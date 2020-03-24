0285e145dffe4aeae9013f6314a3867cf9c43dd7
dupdilan <dupdilan@gmail.com>
2020-03-24 16:14:59 +0530

    some lerning points by 24032020

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
+import { EventInput, Calendar } from '@fullcalendar/core';
+
+
+// import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 
 @Component({
   selector: 'app-calendar',
@@ -8,12 +15,59 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class CalendarComponent implements OnInit {
 
+  // @ViewChild('calendar') calendarComponent: FullCalendarComponent; // the #calendar in the template
+
+  // calendarVisible = true;
+  // calendarWeekends = true;
+  calendarPlugins = [dayGridPlugin,timeGrigPlugin,interactionPlugin]; // important!
+
+  calendarEvents: EventInput = [
+    { title: 'event asd',
+      backgroundColor: 'red',
+      start: new Date(),
+      end: new Date('2020-03-26'),
+      allDay: true
+  },
+    { title: 'event asd',
+      backgroundColor: 'blue',
+      start: new Date('2020-03-17'),
+      end: new Date('2020-03-20'),
+      allDay: true
+  },
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  handleDateClick(arg) {
+    alert("clicked");
+    // console.log(arg.date);
+    // this.calendarEvents = this.calendarEvents.concat({
+    //   title: 'event',
+    //   start: arg.date,
+    //   allDay: arg.allDay
+    // });
+
+  //   this.calendarEvents = this.calendarEvents.push({
+  //      title: 'Test event ',
+  //     backgroundColor: 'yellow',
+  //     start: new Date('2020-03-17'),
+  //     end: new Date('2020-03-20'),
+  //     allDay: true
+  // }
+  //   );
   }
 
 
-  calendarPlugins = [dayGridPlugin]; // important!
+
+  // CalendarEvents = [
+  //   {title: 'car wash', visibleRange: {
+  //     start: '2020-03-04',
+  //     end: '2020-03-05' }
+  //   } ];
 
 }
